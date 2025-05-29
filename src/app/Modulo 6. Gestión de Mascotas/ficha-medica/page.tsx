cdbe000aa72558239c441c28f395c33315200831
Geraldiene <riomahabramromamateo@gmail.com>
2025-05-28 22:14:17 -0400

    v2

---
+++ src/app/Modulo 6. Gestión de Mascotas/ficha-medica/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FichaMeidca() {
+  return (
+    <div>FichaMeidca</div>
+  )
+}
+
+export default FichaMeidca
---
+++ src/app/Modulo 6. Gestión de Mascotas/multimedia/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Multimedia() {
+  return (
+    <div>Multimedia</div>
+  )
+}
+
+export default Multimedia
